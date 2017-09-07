b274fcf7046bdc3937dbf175eb13df76a837240f
Jon <jon@l142519.local>
2017-09-07 15:25:06 -0400

    Initial Commit

---
+++ HardwareDataSpec/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HardwareDataSpec
+//
+//  Created by Jon on 9/7/17.
+//  Copyright © 2017 Jonathan Cardasis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
